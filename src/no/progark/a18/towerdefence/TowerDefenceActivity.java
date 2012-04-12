302abe195cb4c15a949d02459d7f88f835b3fe29
Hanne Oustad <hanneoustad@gmail.com>
2012-04-12 15:54:32 +0200

    Filer for å støtte ny veresjon av Gles 2

--- src/no/progark/a18/towerdefence/TowerDefenceActivity.java
+++ src/no/progark/a18/towerdefence/TowerDefenceActivity.java
@@ -2,7 +2,7 @@ package no.progark.a18.towerdefence;
 
 import org.andengine.engine.camera.Camera;
 import org.andengine.engine.options.EngineOptions;
-import org.andengine.engine.options.EngineOptions.ScreenOrientation;
+import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
